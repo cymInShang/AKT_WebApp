906bbc8ab02e225511a46ad5a2275a6444f1e26c
591546487@qq.com <591546487@qq.com>
2019-09-02 09:41:14 +0800

    update
    login页面

---
+++ AKT_webApp/AKT_webApp/base/AktWkWebViewController.h
@@ -0,0 +1,17 @@
+//
+//  AktWkWebViewController.h
+//  AKT_webApp
+//
+//  Created by 常永梅 on 2019/8/22.
+//  Copyright © 2019 常永梅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AktWkWebViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- AKT_webApp/AKT_webApp/viewcontroller/LoginViewController.m
+++ AKT_webApp/AKT_webApp/viewcontroller/LoginViewController.m
@@ -20,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.btnLogin.layer.masksToBounds = YES;
 }
 
 - (IBAction)btnLoginUserAction:(id)sender {
